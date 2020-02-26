413074b1ff93b585f6cae96a97a630d68b83c824
002400 <zhoupeng@fcbox.com>
2020-02-26 19:58:47 +0800

    add commit

--- data.py
+++ data.py
@@ -42,3 +42,27 @@ def build_map(lists):
             if e not in maps:
                 maps[e] = len(maps)
     return maps
+
+
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        num = []
+        num.append("")
+        num.append("1")
+        if n == 1: return num[1]
+        for i in range(2, n + 1):
+            p = []
+            s = ""
+            for x in num[i - 1]:
+                if p == [] or x == p[0]:
+                    p.append(x)
+                else:
+                    s += str(len(p))
+                    s += p[0]
+                    p = []
+                    p.append(x)
+            s += str(len(p))
+            s += p[0]
+            num.append(s)
+
+        return num[n]
